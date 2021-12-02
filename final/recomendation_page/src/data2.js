d19f214058a87d7f72062212059c9502fb9ac5b4
kieramallinson <79806715+kieramallinson@users.noreply.github.com>
2021-12-02 16:20:54 -0500

    Add files via upload

---
+++ final/recomendation_page/src/data2.js
@@ -0,0 +1,202 @@
+const menu = [
+  {
+    title: "restaurant0",
+    desc: "D",
+    rate: 2,
+    price: "$$$$",
+    vegetarian: "no",
+    catogory: "Italian",
+    img:
+      "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F06%2F23%2Fitalian-food-2000.jpg"
+  },
+  {
+    title: "restaurant1",
+    desc: "C",
+    rate: 3,
+    price: "$",
+    vegetarian: "no",
+    catogory: "chinese",
+    img:
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/11ADE/production/_110541427_chinesefood.jpg"
+  },
+  {
+    title: "restaurant2",
+    desc: "D",
+    rate: 3,
+    price: "$$$$",
+    vegetarian: "no",
+    catogory: "america",
+    img:
+      "https://thumbs.dreamstime.com/z/assorted-american-food-assorted-american-food-hamburger-french-fries-bacon-fried-chicken-172985487.jpg"
+  },
+  {
+    title: "restaurant3",
+    desc: "A",
+    rate: 1,
+    price: "$$$$$",
+    vegetarian: "no",
+    catogory: "chinese",
+    img:
+      "https://assets0.dostuffmedia.com/uploads/aws_asset/aws_asset/6565465/13914f2b-c1ac-4930-b124-c2e29d49afbb.jpg"
+  },
+  {
+    title: "restaurant4",
+    desc: "C",
+    rate: 2,
+    price: "$$$$$",
+    vegetarian: "no",
+    catogory: "mexican",
+    img:
+      "https://media1.fdncms.com/sacurrent/imager/u/original/23149095/taquitos_west_ave.png"
+  },
+  {
+    title: "restaurant5",
+    desc: "B",
+    rate: 1,
+    price: "$$$$",
+    vegetarian: "yes",
+    catogory: "french",
+    img:
+      "https://www.getflavor.com/wp-content/uploads/2019/01/4-Taureaux-Spread.jpg"
+  },
+  {
+    title: "restaurant6",
+    desc: "B",
+    rate: 5,
+    price: "$$$$",
+    vegetarian: "no",
+    catogory: "brazilian",
+    img:
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/barbecued-meats-24426a4.jpg?quality=90&lb=700,350&background=white"
+  },
+  {
+    title: "restaurant7",
+    desc: "C",
+    rate: 3,
+    price: "$$$",
+    vegetarian: "no",
+    catogory: "america",
+    img:
+      "https://media.istockphoto.com/photos/homemade-memorial-day-hamburger-picnic-picture-id531564300?k=20&m=531564300&s=612x612&w=0&h=j8y_MXtEORMI14XvyuzpzrB3msBOwzl-DeWhEHyMFWk="
+  },
+  {
+    title: "restaurant8",
+    desc: "D",
+    rate: 3,
+    price: "$$$$",
+    vegetarian: "no",
+    catogory: "french",
+    img:
+      "https://www.englishclub.com/images/vocabulary/food/french/french-food.jpg"
+  },
+  {
+    title: "restaurant9",
+    desc: "C",
+    rate: 4,
+    price: "$",
+    vegetarian: "no",
+    catogory: "brazilian",
+    img:
+      "https://robbreport.com/wp-content/uploads/2021/09/CABOCO_Credit_DYLAN_JENI.jpg?w=1000"
+  },
+  {
+    title: "restaurant10",
+    desc: "E",
+    rate: 1,
+    price: "$$",
+    vegetarian: "yes",
+    catogory: "brazilian",
+    img: "https://s.hdnux.com/photos/01/20/20/70/21040663/5/1200x0.jpg"
+  },
+  {
+    title: "restaurant11",
+    desc: "B",
+    rate: 5,
+    price: "$$$$$",
+    vegetarian: "no",
+    catogory: "french",
+    img:
+      "https://international.uiowa.edu/sites/international.uiowa.edu/files/wysiwyg_uploads/sa_5.jpg"
+  },
+  {
+    title: "restaurant12",
+    desc: "A",
+    rate: 5,
+    price: "$$",
+    vegetarian: "yes",
+    catogory: "chinese",
+    img:
+      "https://images.ctfassets.net/n3m63hxm9c13/60tqaj5SN5mP4YWAqp6oHT/46a0178d62a0f77c35bd36a6ac9bcd8f/_DSC4756.jpg"
+  },
+  {
+    title: "restaurant13",
+    desc: "B",
+    rate: 4,
+    price: "$",
+    vegetarian: "no",
+    catogory: "chinese",
+    img:
+      "https://www.farwestchina.com/wp-content/uploads/2020/01/healthy-Chinese-food-dishes-1200x675.jpg"
+  },
+  {
+    title: "restaurant14",
+    desc: "A",
+    rate: 2,
+    price: "$",
+    vegetarian: "no",
+    catogory: "indian",
+    img:
+      "https://www.blueosa.com/wp-content/uploads/2020/01/the-best-top-10-indian-dishes.jpg"
+  },
+  {
+    title: "restaurant15",
+    desc: "A",
+    rate: 5,
+    price: "$",
+    vegetarian: "yes",
+    catogory: "french",
+    img:
+      "https://www.goodfoodchannel.org/wp-content/uploads/2016/08/ExpatExplore_macaroons.jpg"
+  },
+  {
+    title: "restaurant16",
+    desc: "C",
+    rate: 5,
+    price: "$$$$$",
+    vegetarian: "yes",
+    catogory: "mexican",
+    img:
+      "https://assets.bonappetit.com/photos/5aa98c2d275dc52331d9189d/1:1/w_3439,h_3439,c_limit/party-posole-rojo.jpg"
+  },
+  {
+    title: "restaurant17",
+    desc: "D",
+    rate: 5,
+    price: "$$$$$",
+    vegetarian: "yes",
+    catogory: "chinese",
+    img:
+      "https://www.thespruceeats.com/thmb/X6mg_2VBCQQ2X8VrLcPTf8_4ce0=/2733x2050/smart/filters:no_upscale()/chinese-take-out-472927590-57d31fff3df78c5833464e7b.jpg"
+  },
+  {
+    title: "restaurant18",
+    desc: "D",
+    rate: 3,
+    price: "$$$$",
+    vegetarian: "yes",
+    catogory: "indian",
+    img:
+      "https://sandinmysuitcase.com/wp-content/uploads/2021/01/Popular-Indian-Food-Dishes.jpg.webp"
+  },
+  {
+    title: "restaurant19",
+    desc: "B",
+    rate: 1,
+    price: "$",
+    vegetarian: "no",
+    catogory: "brazilian",
+    img:
+      "https://blog.amigofoods.com/wp-content/uploads/2019/04/brazilian-foods.jpg"
+  }
+];
+export default menu;
