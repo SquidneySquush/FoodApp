f6577525761b649b9928379a4a48bfba0fb75a22
Haiyu <46879972+SteveXWu@users.noreply.github.com>
2021-12-02 19:27:16 -0500

    Add files via upload

--- final/backend/common/analyze_data.py
+++ final/backend/common/analyze_data.py
@@ -2,29 +2,50 @@ import json
 import pandas as pd
 
 
+def convert_data(result):
+    return str(result).replace("'title'", r'title').replace("'desc'", r'desc').replace("'rate'", r'rate')\
+                            .replace("'price'", r'price').replace("'vegetarian'", r'vegetarian')\
+                            .replace("'catogory'", r'catogory').replace("'img'", r'img').replace("]", "];")
+
+def write_data(result):
+    """title,desc,rate,price,vegetarian,catogory,img"""
+    with open("C:/Users/99596/Desktop/curriculum/SDP/menu-list-master/src/data.js", "w") as f:
+    # with open("./data.js", "w") as f:
+        f.write("const menu = ")
+        result = convert_data(result)
+        f.write(result)
+        f.write("\nexport default menu;")
+
+
 def score_cal(inputs, zone, budget, cravings, preferring):
-  score = 0
-  if zone == inputs[0]:
-    score += 0.4
-  if len(budget) <= len(inputs[1]):
-    score += 0.2
-  if cravings == inputs[2]:
-    score += 0.3
-  if preferring == inputs[3]:
-    score += 0.1
-  return round(score,2)
+    score = 0
+    if zone == inputs[0]:
+        score += 0.4
+    if len(budget) <= len(inputs[1]):
+        score += 0.2
+    if cravings == inputs[2]:
+        score += 0.3
+    if preferring == inputs[3]:
+        score += 0.1
+    return round(score, 2)
 
 
-def analyze(p1,p2,p3,p4):
+def analyze(p1, p2, p3, p4):
+    """ title,desc,rate,price,vegetarian,catogory,img """
     df = pd.read_csv("../dataset/dummy_data.csv", index_col=[0])
     proper_choices = {}
     inputs = [p1, p2, p3, p4]
     for i in range(len(df)):
-        name, zone, rate, budget, preferring, cravings = df.iloc[i].values
+        title, desc, rate, price, vegetarian, catogory, img = df.iloc[i].values
 
-        score = score_cal(inputs, zone, budget, cravings, preferring)
-        proper_choices[name] = score
+        score = score_cal(inputs, desc, price, vegetarian, catogory)
+        proper_choices[str(i)] = score
 
     proper_choices = sorted(proper_choices.items(), key=lambda x: x[1], reverse=True)
-    result = {"r1": proper_choices[0][0], "r2": proper_choices[1][0], "r3": proper_choices[2][0], "r4": proper_choices[3][0]}
-    return json.dumps(result, ensure_ascii= False)
+    result = []
+    for index in proper_choices:
+        title, desc, rate, price, vegetarian, catogory, img = df.iloc[int(index[0])].values
+        result.append({'title': title, 'desc': desc, 'rate': int(rate), 'price': price, 'vegetarian': vegetarian,
+                       'category': catogory, 'img': img})
+    write_data(result)
+    return json.dumps(result, ensure_ascii=False)
