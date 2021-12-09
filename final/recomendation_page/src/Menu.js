19c21a1fee8610758fcec2a0b95d748ffad24d9d
kieramallinson <79806715+kieramallinson@users.noreply.github.com>
2021-12-09 14:30:57 -0500

    Update Menu.js

--- final/recomendation_page/src/Menu.js
+++ final/recomendation_page/src/Menu.js
@@ -4,19 +4,20 @@ const Menu = ({ items }) => {
   return (
     <div className="section-center">
       {items.map((item) => {
-        const { id, title, img, price, desc, rate, catogory, vegetarian} = item;
+        const { id, title, url, img, price, desc, rate, catogory, vegetarian} = item;
         return (
           <article key={id} className="menu-item">
             <img src={img} alt={title} className="photo" />
             <div className="item-info">
               <header>
                 <h4>{title}</h4>
+                <h4><a href={url}>Menu</a></h4>
               </header>
               <p className="item-text">Category: {catogory}</p>
               <p className="item-text">Zone: {desc}</p>
               <p className="item-text">Rating: {rate}</p>
               <p className="item-text">Price: {price}</p>
-              <p className="item-text">{vegetarian}</p>
+              <p className="item-text">Vegetarian Only?: {vegetarian}</p>
             </div>
           </article>
         );
